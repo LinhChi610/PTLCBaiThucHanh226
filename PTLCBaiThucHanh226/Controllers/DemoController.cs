22e70b08cdb794cf26c0f2dedca05c69f57ffdfd
ADMIN <ADMIN@DESKTOP-JE7MIHI>
2022-03-01 10:54:53 +0700

    b1

--- PTLCBaiThucHanh226/Controllers/DemoController.cs
+++ PTLCBaiThucHanh226/Controllers/DemoController.cs
@@ -13,25 +13,5 @@ namespace PTLCBaiThucHanh226.Controllers
         {
             return View();
         }
-        public ActionResult About()
-        {
-            ViewBag.Message = "";
-
-            return View();
-        }
-        [HttpPost]
-
-        public ActionResult Index(String Masinhvien, string HoTensinhvien)
-        {
-            ViewBag.Message = Masinhvien + "   " + HoTensinhvien;
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
